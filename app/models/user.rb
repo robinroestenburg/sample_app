99ecc4d7189673af49ced9ff0c15f75daffaf526
Robin Roestenburg <robinroestenburg@gmail.com>
2011-04-04 22:14:46 +0200

    User model validations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 # == Schema Information
-# Schema version: 20110403175537
+# Schema version: 20110404201143
 #
 # Table name: users
 #
@@ -14,4 +14,13 @@ class User < ActiveRecord::Base
   
   attr_accessible :name, :email
   
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
+  validates :name,  :presence => true, 
+                    :length   => { :maximum => 50 }
+                    
+  validates :email, :presence   => true,
+                    :format     => { :with => email_regex },
+                    :uniqueness => { :case_sensitive => false }
+    
 end
