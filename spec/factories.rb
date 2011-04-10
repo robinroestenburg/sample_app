aa9613512d6cb7c5c313adb4459a5ce2c9a30c27
Robin Roestenburg <robinroestenburg@gmail.com>
2011-04-10 12:51:05 +0200

    Added a User factory and tests for the show page.

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user| 
+  user.name                  "Robin Roestenburg"
+  user.email                 "robinroestenburg@gmail.com"
+  user.password              "foobar"
+  user.password_confirmation "foobar"
+end
