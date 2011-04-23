9b3db280616a042c701439233a1f29d8a75d8e6c
Robin Roestenburg <robinroestenburg@gmail.com>
2011-04-23 14:06:11 +0200

    Friendly forwarding and correct user requirement.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
       render 'new'
     else
       sign_in user
-      redirect_to user
+      redirect_back_or user
     end
   end
   
@@ -25,7 +25,5 @@ class SessionsController < ApplicationController
     sign_out
     redirect_to root_path
   end
-
-  
 
 end
